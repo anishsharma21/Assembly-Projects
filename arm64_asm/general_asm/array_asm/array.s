6452405107cbe4a191e2b90e6929ed0111b1e0b3
anishsharma21 <anisharma2103@gmail.com>
2024-08-25 12:00:47 +1000

    Compiling a C program to understand how compilation is done by the gcc, and also to reverse engineer how ARM64 arrays are created by looking at the assembly program output

---
+++ arm64_asm/general_asm/array_asm/array.s
@@ -0,0 +1,52 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #64
+	.cfi_def_cfa_offset 64
+	stp	x29, x30, [sp, #48]             ; 16-byte Folded Spill
+	add	x29, sp, #48
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	stur	x8, [x29, #-8]
+	str	wzr, [sp, #12]
+	adrp	x8, l___const.main.arr@PAGE
+	add	x8, x8, l___const.main.arr@PAGEOFF
+	ldr	q0, [x8]
+	str	q0, [sp, #16]
+	ldr	w8, [x8, #16]
+	str	w8, [sp, #32]
+	ldur	x9, [x29, #-8]
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	subs	x8, x8, x9
+	cset	w8, eq
+	tbnz	w8, #0, LBB0_2
+	b	LBB0_1
+LBB0_1:
+	bl	___stack_chk_fail
+LBB0_2:
+	mov	w0, #1
+	ldp	x29, x30, [sp, #48]             ; 16-byte Folded Reload
+	add	sp, sp, #64
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__const
+	.p2align	2, 0x0                          ; @__const.main.arr
+l___const.main.arr:
+	.long	1                               ; 0x1
+	.long	2                               ; 0x2
+	.long	3                               ; 0x3
+	.long	4                               ; 0x4
+	.long	5                               ; 0x5
+
+.subsections_via_symbols
