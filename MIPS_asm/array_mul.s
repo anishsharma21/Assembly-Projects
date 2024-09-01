b5c04e4b915b5bc6a1d72e2a3ce83acd6508c3e2
anishsharma21 <anisharma2103@gmail.com>
2024-09-01 20:08:50 +1000

    It works now, generic program that is a little verbose and can probably be tidied up, but it multiplies all the elements in an array without using the mult operation, just loops and conditional branching and jumping, made lots of logical mistakes like squaring or adding incorrectly before, but finally got it to work once I understand fundamentally what needed to be done, all written in vim too

--- MIPS_asm/array_mul.s
+++ MIPS_asm/array_mul.s
@@ -8,25 +8,31 @@ newline: .asciiz "\n"
 .globl main
 
 main:
-    li $t0, 1                   # running sum
-    li $t1, 0                   # current index
+    li $t1, 1                   # current index
     la $t2, array               # address of array
     lw $t3, 0($t2)              # length of array
+    addi $t2, 4                 # set first value because multiplication
+    lw $t0, 0($t2)              # loading initial value for final output
     j loop
 
 loop:
     addi $t2, 4
     addi $t1, 1
     lw $t4, 0($t2)
+    add $t5, $t0, $zero         # copy of current value to add
+    addi $t4, -1
     j multiply
 
 multiply:
-    add $t0, $t0, $t0
+    beq $t4, $zero, check
+    add $t0, $t0, $t5
     addi $t4, -1
-    bgtz $t4, multiply
+    j multiply
+
+check:
     beq $t1, $t3, printsum
     j loop  
-
+    
 printsum:
     li $v0, 1
     move $a0, $t0
