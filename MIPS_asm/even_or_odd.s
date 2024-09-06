eefa3f6a5e904955e4e4ed59b7ce9c6dc2544ec8
anishsharma21 <anisharma2103@gmail.com>
2024-09-06 19:41:31 +1000

    program that checks if a value is even or odd, next iteratation will do this for an entire array, treating the current program logic as a procedure

--- MIPS_asm/even_or_odd.s
+++ MIPS_asm/even_or_odd.s
@@ -1,11 +1,34 @@
 .data
 .align 2
 
-newline: "\n"
+even: .asciiz " is even\n"
+odd: .asciiz " is odd\n"
 value: .word 23
 
 .text
 .globl main
 
 main:
-    
+    la $t0, value
+    lw $t0, 0($t0)
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    li $t1, 2
+    div $t0, $t1
+    mfhi $a0                # remainder
+    mflo $a1                # quotient
+    li $v0, 4
+    beq $a0, 0, printeven
+    la $a0, odd
+    syscall
+    j end
+
+printeven:
+    la $a0, even
+    syscall
+    j end
+
+end:
+    li $v0, 10
+    syscall 
