84fd00c38351e73055b7be81239fdccf084f1bc9
anishsharma21 <anisharma2103@gmail.com>
2024-09-07 14:57:51 +1000

    assembly program that takes user input, fills an array of integers, then finds the maximum value in that array, neatly presented with other ascii strings with a prompt and output

--- MIPS_asm/find_max.s
+++ MIPS_asm/find_max.s
@@ -2,24 +2,53 @@
 .align 2
 
 newline: .asciiz "\n"
-array: .word 5, 2, 4, 99, -8, 23
+
+.align 2
+array: .space 24                            # 6 word spaces, 1 for length, 5 for values
+
+prompt: .asciiz "Type in 5 numbers:\n"
+max_string: .asciiz "Max value: "
 
 .text
 .globl main
 
 main:
-    la $a2, array               # load address of array
-    lw $a1, ($a2)               # length of array
-    addi $a2, 4                 # next value
-    lw $a0, ($a2)               # set first value to max
+    # beginning with user input
+    la $a2, array
+    li $t0, 5
+    sw $t0, ($a2)                           # storing the length first
+    la $a0, prompt                          # print prompt
+    li $v0, 4
+    syscall
+    jal user_input_loop
+
+    la $a2, array                           # load address of array
+    lw $a1, ($a2)                           # length of array
+    addi $a2, 4                             # next value
+    lw $a0, ($a2)                           # set first value to max
     addi $a1, -1
     jal find_max
+    move $t0, $a0
+    la $a0, max_string
+    li $v0, 4
+    syscall
+    move $a0, $t0
     li $v0, 1
     syscall
     la $a0, newline
     li $v0, 4
     syscall
     j end
+
+user_input_loop:
+    # ask user input 5 times, fill up array, then return
+    li $v0, 5
+    syscall
+    addi $a2, 4
+    sw $v0, ($a2)
+    addi $t0, -1
+    bgt $t0, $zero, user_input_loop
+    jr $ra
 
 find_max:
     addi $a2, 4
