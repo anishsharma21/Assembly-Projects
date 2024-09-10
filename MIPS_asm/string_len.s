c12ffa1b4fed2a34b50ffb9fda831f3b4ba8da48
anishsharma21 <anisharma2103@gmail.com>
2024-09-10 14:45:12 +1000

    assembly program now takes in user input string, creates buffer space for the string, then prints each ascii encoded byte of the string and length of string at the end, interesting requirements and implementations, I wonder if dynamic allocation of a buffer is possible or not

--- MIPS_asm/string_len.s
+++ MIPS_asm/string_len.s
@@ -1,27 +1,75 @@
 .data
-.align 2
 
-string:
-    .ascii "hello world"
-    .byte 0
-
+buffer: .space 20
+str1: .asciiz "Enter string: "
+str2: .asciiz "You wrote: "
+str3: .asciiz "Length of string: "
+str4: .asciiz "byte #"
+sep: .asciiz ": "
 newline: .asciiz "\n"
 
 .text
-.globl main
 
 main:
-    la $a1, string
+    la $a0, str1
+    li $v0, 4
+    syscall
+
+    li $v0, 8                   # read string input
+    la $a0, buffer
+    li $a1, 20
+    syscall
+
+    la $a0, str2
+    li $v0, 4
+    syscall
+
+    la $a0, buffer
+    li $v0, 4
+    syscall
+
+    la $a0, newline
+    li $v0, 4
+    syscall
+
+    la $a0, buffer
+    move $a1, $a0
     li $a2, 0
+    j find_length
 
 find_length:
     lb $a0, ($a1)
     beqz $a0, end
+    move $t0, $a0
+
+    la $a0, str4
+    syscall
+    move $a0, $a2
+    li $v0, 1
+    syscall
+    la $a0, sep
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1                   # print byte
+    syscall
+    la $a0, newline
+    li $v0, 4
+    syscall
+
     addi $a2, $a2, 1
     addi $a1, $a1, 1
     j find_length
 
 end:
+    la $a0, newline
+    li $v0, 4
+    syscall
+
+    la $a0, str3
+    li $v0, 4
+    syscall
+
     move $a0, $a2
     li $v0, 1
     syscall
