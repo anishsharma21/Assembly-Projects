b4319163091490f4358e1fbd98f4fb117945229f
anishsharma21 <anisharma2103@gmail.com>
2024-09-12 14:11:00 +1000

    further progress on bubble sort program, messed up the logic so will need to do some refactoring of the code

--- MIPS_asm/bubble_sort.s
+++ MIPS_asm/bubble_sort.s
@@ -21,6 +21,34 @@ main:
 
     j end
 
+# a2 is cur idx, $t0 is next idx (for swap)
+bubble_sort:
+    beq $a2, $a1, end_sort                                  # end sort if cur idx == len arr
+    add $t0, $a2, 1                                         # otherwise, set next idx
+    bgt $t0, $a1, update_cur_idx                            # reset next idx if > than arr len
+
+    sll $a2, $a2, 2                                         # word length for cur idx
+    sll $t0, $t0, 2                                         # word length for next idx
+    add $t1, $a0, $a2                                       # cur idx mem addr
+    add $t2, $a0, $t0                                       # next idx mem addr
+    j swap
+
+update_cur_idx:
+    addi $a2, 1
+    move $t0, $a2
+    j bubble_sort
+
+swap:
+    lw $t3, ($t1)
+    lw $t4, ($t2)
+    sw $t3, ($t2)
+    sw $t4, ($t1)
+    addi $t0, 1
+    j bubble_sort
+
+end_sort:
+    jr $ra
+
 print_arr:
     move $t0, $a0
     lw $a0, ($a0)
