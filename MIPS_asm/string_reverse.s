cdc85d1222a0a9e670c1ac35e9f7f1ee4219c238
anishsharma21 <anisharma2103@gmail.com>
2024-09-13 22:58:22 +1000

    getting my commit in for the day, busy day, just printing all the bytes of a string

--- MIPS_asm/string_reverse.s
+++ MIPS_asm/string_reverse.s
@@ -30,3 +30,20 @@ main:
 
     li $v0, 10
     syscall
+
+print_str_dec:
+    lb $t0, ($a0)
+    beq $t0, $zero, jump_main
+    move $t1, $a0
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    la $a0, newline
+    li $v0, 4
+    syscall
+    move $a0, $t1
+    addi $a0, 1
+    j print_str_dec
+
+jump_main:
+    jr $ra
