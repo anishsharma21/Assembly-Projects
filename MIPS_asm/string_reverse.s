2fb71b396349f47bfd5c867a36ddeaece350475d
anishsharma21 <anisharma2103@gmail.com>
2024-09-13 23:45:42 +1000

    correct previous commit, printing bytes with the actual procedure being called

--- MIPS_asm/string_reverse.s
+++ MIPS_asm/string_reverse.s
@@ -16,17 +16,14 @@ main:
     li $v0, 8
     syscall
 
+    # print inputted string
     la $a0, buffer
     li $v0, 4
     syscall
 
-    lb $a0, ($a0)
-    li $v0, 1
-    syscall
-
-    la $a0, newline
-    li $v0, 4
-    syscall
+    la $a0, buffer
+    li $t2, 10                      # ascii encoding for line feed, checked later in code
+    jal print_str_dec
 
     li $v0, 10
     syscall
@@ -34,6 +31,8 @@ main:
 print_str_dec:
     lb $t0, ($a0)
     beq $t0, $zero, jump_main
+    beq $t0, $t2, jump_main
+
     move $t1, $a0
     move $a0, $t0
     li $v0, 1
@@ -41,6 +40,7 @@ print_str_dec:
     la $a0, newline
     li $v0, 4
     syscall
+
     move $a0, $t1
     addi $a0, 1
     j print_str_dec
