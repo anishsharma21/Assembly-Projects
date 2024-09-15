a56543c0df7bf077370f6fad7e4bf7e418d615e4
anishsharma21 <anisharma2103@gmail.com>
2024-09-15 18:34:24 +1000

    MIPS assembly program, next stage of program that checks if user inputted string matches string in static memory

--- MIPS_asm/string_compare.s
+++ MIPS_asm/string_compare.s
@@ -2,10 +2,9 @@
 
 string: .asciiz "hello"
 buffer: .space 100
-prompt: .asciiz "What do you think the string is: "
-incorrect: .asciiz "Incorrect! Try again..."
+prompt: .asciiz "The string is: "
+incorrect: .asciiz "Incorrect! Try again...\n"
 correct: .asciiz "Correct!"
-newline: .asciiz "\n"
 
 .text
 
@@ -19,4 +18,32 @@ main:
     li $v0, 8
     syscall
 
+    li $v0, 4
+    la $a0, buffer
+    la $a1, string
+    j compare_str
 
+compare_str:
+    lw $t0, ($a0)
+    lw $t1, ($a1)
+    beq $t0, 10, end_str_check                  # when line feed found
+    addi $a0, 4
+    addi $a1, 4
+    bne $t0, $t1, incorrect_str
+    j compare_str
+
+end_str_check:
+    beq $t1, 0, correct_str
+    j incorrect_str
+
+correct_str:
+    la $a0, correct
+    syscall
+
+    li $v0, 10
+    syscall
+
+incorrect_str:
+    la $a0, incorrect
+    syscall
+    j main
