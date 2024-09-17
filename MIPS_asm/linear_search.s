bf0c2cc0941eef9b470b77c5910cebeae1a429f9
anishsharma21 <anisharma2103@gmail.com>
2024-09-17 19:03:47 +1000

    MIPS assembly program, completed linear search program, prints array, takes in user inputs, determine index of user inputted integer, not found accounted for, loops until 0 is inputted

--- MIPS_asm/linear_search.s
+++ MIPS_asm/linear_search.s
@@ -1,6 +1,6 @@
 .data
 
-array: .word 10, 2, -5, 8, 33, 2, 19, 11, 9, 3, 0
+array: .word 10, 2, -5, 8, 33, 2, 19, 11, 9, 3, 7
 arrayStr: .asciiz "Array: "
 prompt: .asciiz "Find index of: "
 index: .asciiz "Index of '"
@@ -22,8 +22,59 @@ main:
     addi $a0, 4
     jal printArr
 
-    li $v0, 10
+    la $a0, prompt
     syscall
+
+    # read int
+    li $v0, 5
+    syscall
+
+    beq $v0, 0, exit
+
+    la $a0, array
+    move $a1, $v0                       # user input int
+    li $a2, 0                           # cur idx
+    lw $a3, ($a0)                       # len of array
+    addi $a0, 4
+    jal linearSearch
+
+    j main
+
+linearSearch:
+    lw $t0, ($a0)
+    beq $t0, $a1, found
+    addi $a2, 1
+    addi $a0, 4
+    bne $a2, $a3, linearSearch
+
+    la $a0, notFound
+    li $v0, 4
+    syscall
+    la $a0, newline
+    syscall
+    jr $ra
+
+found:
+    la $a0, index
+    li $v0, 4
+    syscall
+
+    move $a0, $t0
+    li $v0, 1
+    syscall
+
+    la $a0, is
+    li $v0, 4
+    syscall
+
+    move $a0, $a2
+    li $v0, 1
+    syscall
+
+    la $a0, newline
+    li $v0, 4
+    syscall
+    jr $ra
 
 printArr:
     move $t0, $a0
@@ -47,3 +98,7 @@ printSep:
     move $a0, $t0
     addi $a0, 4
     j printArr
+
+exit:
+    li $v0, 10
+    syscall
