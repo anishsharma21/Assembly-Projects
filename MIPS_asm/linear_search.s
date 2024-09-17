55760a66029900b25d5fb4a1aba276f85b0f49b5
anishsharma21 <anisharma2103@gmail.com>
2024-09-17 18:51:31 +1000

    MIPS assembly program, implementing linear search, stage 1

--- MIPS_asm/linear_search.s
+++ MIPS_asm/linear_search.s
@@ -1,4 +1,49 @@
 .data
 
 array: .word 10, 2, -5, 8, 33, 2, 19, 11, 9, 3, 0
+arrayStr: .asciiz "Array: "
 prompt: .asciiz "Find index of: "
+index: .asciiz "Index of '"
+is: .asciiz "' is: "
+notFound: .asciiz "Not found"
+newline: .asciiz "\n"
+sep: .asciiz ", "
+
+.text
+
+main:
+    la $a0, arrayStr
+    li $v0, 4
+    syscall
+
+    la $a0, array
+    li $a1, 0
+    lw $a2, ($a0)
+    addi $a0, 4
+    jal printArr
+
+    li $v0, 10
+    syscall
+
+printArr:
+    move $t0, $a0
+    lw $a0, ($a0)
+    li $v0, 1
+    syscall
+
+    addi $a1, 1
+    bne $a1, $a2, printSep
+
+    la $a0, newline
+    li $v0, 4
+    syscall
+    jr $ra
+
+printSep:
+    la $a0, sep
+    li $v0, 4
+    syscall
+
+    move $a0, $t0
+    addi $a0, 4
+    j printArr
