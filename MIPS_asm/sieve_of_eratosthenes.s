ec0bd7aca2b0ce9e8362b4cfa345661444fc0d71
anishsharma21 <anisharma2103@gmail.com>
2024-09-18 16:51:58 +1000

    MIPS assembly program, stage 3 of sieve of eratosthenes, coding out the main proc's of the program, setting the jump val, incrementing and zeroing out multiples of the jump val in the buffer, handling end cases, finding next jump vals, handling the 1 value (unique case that must be handled first each time) - wholistic approach doens't seem to be the most efficient or neat, but attempting to solve the problem first before refactoring later

--- MIPS_asm/sieve_of_eratosthenes.s
+++ MIPS_asm/sieve_of_eratosthenes.s
@@ -31,14 +31,47 @@ main:
     jal fillBuffer
 
     la $a0, buffer                                  # addr of sieve buffer
-    lw $a1, ($a0)                                   # length
+    lw $a1, ($a0)                                   # final idx (mutates)
     addi $a0, 4
+    li $a2, 2                                       # jump val (mutates)
 
     j end
 
 # start with 2, add to base addr, set those values to 0, during loop iteration check if value already set to 0, if it is, increment by 2 and next iteration, end of loop iteration, find next val for jump val in buffer (next val not set to 0), then repeat that again with next jump val, repeat until jump val cannot be found (end of buffer reached), then final run through the buffer, printing values with separations that aren't equal to 0, those are the primes, newline, back to main
 sieve:
-    # TODO
+    bgt $a2, $a1, printPrimes                       # jump val >= final idx
+    move $t0, $a1                                   # copy final idx
+    beq $a2, 2, handle1n2                           # first iteration is weird, handle 1 and 2 jump vals
+    sll $t1, $a2, 2                                 # jump val set to word length
+    move $t2, $a1                                   # cur idx that will be updated each iter
+    j sieveJumps                                    # null out all multiples of jump vals
+
+sieveJumps:
+    sw $zero, ($t0)                                 # set word at cur addr to 0
+    add $t0, $t0, $t1                               # next addr incr by sll jump val
+    add $t2, $t2, $a2                               # increment cur idx
+    blt $t2, $a1, sieveJumps                        # while cur idx <= final idx
+    j nextJumpVal
+
+nextJumpVal:
+    move $t0, $a0                                   # copy of base addr (first val)
+    add $t0, $t0, $t1                               # incr addr by sll jump val
+    move $t2, $a2                                   # copy of cur jump val
+    j nextJumpValLoop
+
+nextJumpValLoop:
+    addi $t0, 4                                     # addr of next potential jump val
+    addi $t2, 1                                     # incr cur jump val to next
+    bgt $t2, $a1, printPrimes                       # if cur jump val > final idx, sieve finished
+    lw $t1, ($t0)                                   # load next jump val
+    beq $t1, $zero, nextJumpValLoop                 # if jump val has been set to 0, go to next
+    move $a2, $t1                                   # update jump val to next val
+    j sieve
+
+handle1n2:
+    sw $zero, ($t0)                                 # set the value 1 to 0 immediately
+    addi $a0, 4                                     # 1 value handled, move to 2 jump val and handle
+    j sieveJumps
 
 fillBuffer:
     addi $t0, $a2, 1
