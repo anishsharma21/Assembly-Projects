3658e2d15892aeb8de5d3c5b3b747efc7ebc1e08
anishsharma21 <anisharma2103@gmail.com>
2024-09-18 14:57:43 +1000

    MIPS assembly program, stage 2 of sieve of eratosthenes, error handling procedures to gracefully loop back to main, also to end the program when 0 is inputted from prompt, fillBuffer proc to set the buffer array up with the right values

--- MIPS_asm/sieve_of_eratosthenes.s
+++ MIPS_asm/sieve_of_eratosthenes.s
@@ -10,7 +10,7 @@ NegativeValueErrorStr: .asciiz "\nNumber must be >0\n\n"
 
 .text
 
-# find prime numbers up to user input integer value (max is 256 for now), then buffer is filled with numbers up until that value, then sieve function called, all places where values are cut out, those values are set to 0, so on the last run through of the buffer, the values still present are printed in order
+# find prime numbers up to user input integer value (max is 255 for now), then buffer is filled with numbers up until that value, then sieve function called, all places where values are cut out, those values are set to 0, so on the last run through of the buffer, the values still present are printed in order
 
 main:
     la $a0, prompt
@@ -19,13 +19,34 @@ main:
 
     li $v0, 5
     syscall
-    blt $v0, 0, NegativeValueError
     beq $v0, 0, end
-    bge $v0, 256, MaxValueError
+    blt $v0, 0, NegativeValueError
+    bge $v0, 255, MaxValueError                     # >= bc 1 val required for length
 
-    move $a0, $v0
-    la $a1, buffer
+    move $a0, $v0                                   # max idx
+    la $a1, buffer                                  # buffer addr
+    li $a2, 0                                       # cur idx
+    sw $a0, ($a1)
+    addi $a1, 4
     jal fillBuffer
+
+    la $a0, buffer                                  # addr of sieve buffer
+    lw $a1, ($a0)                                   # length
+    addi $a0, 4
+
+    j end
+
+# start with 2, add to base addr, set those values to 0, during loop iteration check if value already set to 0, if it is, increment by 2 and next iteration, end of loop iteration, find next val for jump val in buffer (next val not set to 0), then repeat that again with next jump val, repeat until jump val cannot be found (end of buffer reached), then final run through the buffer, printing values with separations that aren't equal to 0, those are the primes, newline, back to main
+sieve:
+    # TODO
+
+fillBuffer:
+    addi $t0, $a2, 1
+    sw $t0, ($a1)
+    addi $a2, 1
+    addi $a1, 4
+    bne $a2, $a0, fillBuffer
+    jr $ra
 
 NegativeValueError:
     la $a0, NegativeValueErrorStr
