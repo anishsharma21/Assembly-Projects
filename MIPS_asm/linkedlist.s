de568eb62d6a62dcd49d8ee13591b3c98e88e6a0
anishsharma21 <anisharma2103@gmail.com>
2024-09-21 16:18:19 +1000

    MIPS assembly program, stage 3 of linked list, commiting frequently and early, just making sure my current changes work and are saved before moving forward, want to maintain these changes and be able to revert back if I make breaking changes that are unrevertable

--- MIPS_asm/linkedlist.s
+++ MIPS_asm/linkedlist.s
@@ -2,7 +2,7 @@
 
 .data
 
-userchoice: .word 4
+userchoice: .space 1
 
 intro: .asciiz "Linked List initialised."
 choice1: .asciiz "\nChoose head (h), next (n), val (v), or push (p)\n"
@@ -12,6 +12,7 @@ nextstr: .asciiz "You are at the next node"
 pushedstr: .asciiz "Pushed node"
 valstr: .asciiz "The value of this node is: "
 
+choiceerr: .asciiz "Invalid choice. Choose from h, n, v, or p."
 nexterrstr: .asciiz "There is no next value."
 valerrstr: .asciiz "This node has no value"
 newline: .asciiz "\n"
@@ -27,11 +28,17 @@ main:
     syscall                                     # read str, setup in displayOptions
 
     la $a0, userchoice
+    lb $a0, ($a0)
+    # switch statement
+    beq $a0, 72, goToHead
+    beq $a0, 78, goToNext
+    beq $a0, 86, showVal
+    beq $a0, 80, pushNode
+
+    la $a0, choiceerr
     li $v0, 4
     syscall
-
-    li $v0, 10
-    syscall
+    j main
 
 displayOptions:
     la $a0, choice1
@@ -41,5 +48,5 @@ displayOptions:
     syscall
     li $v0, 8                                   # read str
     la $a0, userchoice                          # buffer for choice char
-    li $a1, 4                                   # len of choice str (1 char)
+    li $a1, 1                                   # len of choice str (1 char/byte)
     jr $ra
