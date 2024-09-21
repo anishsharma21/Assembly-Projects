d09a6b586f54c1a922855971c58613cb52bee620
anishsharma21 <anisharma2103@gmail.com>
2024-09-21 16:09:37 +1000

    MIPS assembly program, stage 2 of linked list, reading str from user, printing it

--- MIPS_asm/linkedlist.s
+++ MIPS_asm/linkedlist.s
@@ -2,14 +2,44 @@
 
 .data
 
-str1: .asciiz "Linked List initialised."
-str2: .asciiz "Choose head (h), next (n), or val (v): "
+userchoice: .word 4
+
+intro: .asciiz "Linked List initialised."
+choice1: .asciiz "\nChoose head (h), next (n), val (v), or push (p)\n"
+choice2: .asciiz "Choice: "
 headstr: .asciiz "You are at the head node"
 nextstr: .asciiz "You are at the next node"
+pushedstr: .asciiz "Pushed node"
 valstr: .asciiz "The value of this node is: "
+
 nexterrstr: .asciiz "There is no next value."
 valerrstr: .asciiz "This node has no value"
+newline: .asciiz "\n"
 
 .text
 
 main:
+    la $a0, intro
+    li $v0, 4
+    syscall
+
+    jal displayOptions
+    syscall                                     # read str, setup in displayOptions
+
+    la $a0, userchoice
+    li $v0, 4
+    syscall
+
+    li $v0, 10
+    syscall
+
+displayOptions:
+    la $a0, choice1
+    li $v0, 4
+    syscall
+    la $a0, choice2
+    syscall
+    li $v0, 8                                   # read str
+    la $a0, userchoice                          # buffer for choice char
+    li $a1, 4                                   # len of choice str (1 char)
+    jr $ra
