6b3911e886359dea6342eb2fb228732da8acb654
anishsharma21 <anisharma2103@gmail.com>
2024-09-21 17:19:55 +1000

    MIPS assembly program, stage 5 of linked list, newlines for strings so proper formatting, jumping to main loop for choices given, quit option added

--- MIPS_asm/linkedlist.s
+++ MIPS_asm/linkedlist.s
@@ -3,22 +3,23 @@
 .data
 
 userchoice: .space 4
+headNode: .space 8
 headNodeAddr: .space 4
 curNodeAddr: .space 4
 
-intro: .asciiz "Linked List initialised."
-choice1: .asciiz "\nChoose head (h), next (n), val (v), or push (p)\n"
+intro: .asciiz "\nLinked List initialised.\n"
+choice1: .asciiz "\nChoose head (h), next (n), val (v), push (p), or quit (q)\n"
 choice2: .asciiz "Choice: "
 
-headstr: .asciiz "You are at the head node\n"
-nextstr: .asciiz "You are at the next node\n"
-pushedstr: .asciiz "Pushed node\n"
-valstr: .asciiz "The value of this node is: "
+headstr: .asciiz "\nYou are at the head node\n"
+nextstr: .asciiz "\nYou are at the next node\n"
+pushedstr: .asciiz "\nPushed node\n"
+valstr: .asciiz "\nThe value of this node is: "
 newline: .asciiz "\n"
 
-choiceerr: .asciiz "Invalid choice. Choose from h, n, v, or p."
-nexterrstr: .asciiz "There is no next value."
-valerrstr: .asciiz "This node has no value"
+choiceerr: .asciiz "\nInvalid choice. Choose from h, n, v, or p.\n"
+nexterrstr: .asciiz "\nThere is no next value.\n"
+valerrstr: .asciiz "\nThis node has no value\n"
 
 .text
 
@@ -42,6 +43,7 @@ mainLoop:
     beq $a0, 110, goToNext
     beq $a0, 118, showVal
     beq $a0, 112, pushNode
+    beq $a0, 113, quit
 
     la $a0, choiceerr
     li $v0, 4
@@ -63,19 +65,25 @@ goToNext:
     la $a0, nextstr
     li $v0, 4
     syscall
-    jr $ra
+
+    j mainLoop
 
 showVal:
     la $a0, valstr
     li $v0, 4
     syscall
-    jr $ra
+
+    la $a0, curNodeAddr
+    li $v0, 1
+    syscall
+
+    j mainLoop
 
 pushNode:
     la $a0, pushedstr
     li $v0, 4
     syscall
-    jr $ra
+    j mainLoop
 
 displayOptions:
     la $a0, choice1
@@ -96,10 +104,13 @@ initHeadNode:
     li $a0, 8                                   # 4 bytes for val, 4 bytes for next addr
     li $v0, 9                                   # sbrk, returns addr in v0
     syscall
+
     la $a0, headNodeAddr                        # store head node addr in headNodeAddr
     sw $v0, ($a0)
     la $a0, curNodeAddr
     sw $v0, ($a0)                               # store head node addr in curNodeAddr
     jr $ra
 
-
+quit:
+    li $v0, 10
+    syscall
