83b63a36af01f81cb8156a3beedb446df6e0b064
anishsharma21 <anisharma2103@gmail.com>
2024-09-24 18:03:27 +1000

    MIPS assembly program, stage 1 of malloc/free program, I am attempting to write a memory manager in MIPS assembly using the SPIM simulator. I will do a basic implementation of memory allocation and freeing by asking the user for the name of a variable they would like to allocate, and additional space they would like to store on it, then the block of memory will be allocated with a byte at the start for the length of the entire memory block, then the ascii encoding of the name, and finally the additional space in bytes.
    I wanted to hold the memory addresses of allocated memory blocks in one array, and memory reuse addresses in another with their lengths. I will then sort this second arr by the lengths so that binary search can be used to find the best fit length of memory for future allocation. These ideas and more have been documented in the file currently to help guide development - from my experience thus far, refactoring changes later into development is difficult given the complexity of assembly programming, state flow, and the debugging process, so doing some preliminary planning is useful to mitigate at least some of the problems I will encounter

---
+++ MIPS_asm/malloc_free.s
@@ -0,0 +1,13 @@
+# thinking through my implementation here: 
+# - user types in name for variable, then they enter integer for number of bytes of space for it - keeping things super simple to start with
+# - initially, simple sbrk syscall, allocate space, store size meta data too (for now), and the name of the variable itself (which also acts as its value) - memory allocation is a byte for the length of the memory block, followed by ascii encoding of the variable name, and then finally the additional space as specified in bytes
+# - then, returned address is a pointer to this newly allocate memory space - this needs to be stored in a dynamically allocated array (no point in storing it statically because resizing will be necessary - and also, when values arr copied into a separate array when resizing, the previous memory space can be used for other data items too)
+# - then, the user can choose between allocating or freeing from the second step onwards
+# - if the user chooses to free, they are presented with the names of all the variables they can choose to free, then, the user input is matched to one of the variables, if there is no match then the loop repeats for allocation or freeing, otherwise, the memory manager sets the address to free and returns the length of that array too. NOTE these are then kept in a separate array for saved memory spaces - useful for future use
+# - when deallocation occurs, the pointer and length are stored in another memory reuse arr and can then be checked when making future allocations - the logic is to go through this array and find the first pointer to an address with sufficient space
+
+# Future:
+# - Sort the memory reuse arr so that we can use binary search to find the ideal length in O(logn) time
+# - Instead of searching the allocated-list for the names that match the inputted variable name for freeing, we should keep a hash potentially of each value that is stored in a byte
+
+.data
